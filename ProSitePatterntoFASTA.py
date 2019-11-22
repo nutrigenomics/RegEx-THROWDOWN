edce3ba9199b705a11fca3f62f98dfc65a4bd3a3
nutrigenomics <56320113+nutrigenomics@users.noreply.github.com>
2019-11-21 20:48:13 -0500

    Update ProSitePatterntoFASTA.py

--- ProSitePatterntoFASTA.py
+++ ProSitePatterntoFASTA.py
@@ -6,15 +6,26 @@ import requests
 
 import re
 
+User1 = input("USER 1: Please enter your name: ")
+User2 = input("USER 2: Please enter your name: ")
+
+print("\n\nWelcome", User1," and", User2,"TO REGEX THROWDOWN!\n\n")
+
 regex1 = input("USER 1: Enter your ProSite formatted pattern: ")
+regex2 = input("USER 2: Enter your ProSite formatted pattern: ")
 
 url1 = "https://prosite.expasy.org/cgi-bin/prosite/PSScan.cgi?sig="+regex1
+url2 = "https://prosite.expasy.org/cgi-bin/prosite/PSScan.cgi?sig="+regex2
 
-r  = requests.get(url1)
+r1  = requests.get(url1)
+r2  = requests.get(url2)
 
-data = r.text
+data1 = r1.text
+data2 = r2.text
 
-soup = str(BeautifulSoup(data))
+soup1 = str(BeautifulSoup(data1))
+soup2 = str(BeautifulSoup(data2))
+
 
 #print("\n*********************\n\nHere are the Prosite search results based on your regular expression:\n*********************\n\n")
 #print(soup)
@@ -27,64 +38,94 @@ import re
 
 #souplist = re.findall(r"\|[a-zA-Z0-9]*?[\-]*?[a-zA-Z0-9]*?[\-|\|]", soup)
 
-souplist = re.findall(r"\|[a-zA-Z0-9]*?\|", soup)
+souplist1 = re.findall(r"\|[a-zA-Z0-9]*?\|", soup1)
+souplist2 = re.findall(r"\|[a-zA-Z0-9]*?\|", soup2)
 
 # Delete the last two entries which are "||" from the HTML of that same webpage.
     
-del souplist[-1]
-del souplist[-1]
+del souplist1[-1]
+del souplist1[-1]
+del souplist2[-1]
+del souplist2[-1]
 
 # Strip out | and - which were initially kept as part of the regular expression to isolate gene names
 
-striplist = ([s.replace('|', '') for s in souplist]) # remove the | from the string borders
-striplist = ([s.replace('-', '') for s in striplist])
+striplist1 = ([s.replace('|', '') for s in souplist1]) # remove the | from the string borders
+striplist1 = ([s.replace('-', '') for s in striplist1])
+
+striplist2 = ([s.replace('|', '') for s in souplist2]) # remove the | from the string borders
+striplist2 = ([s.replace('-', '') for s in striplist2])
 
 print("\n\n\n")
 
-print("\n\nUSER 1: Here is the list of genes matched by your submitted ProSite pattern, ignoring isoforms. \nWe will be using only the first 5 best matches to construct your phylogenetic tree.\n\n")
-for gene in striplist:
-    print(gene)
+print("\n\nUSER 1: Here is the list of genes matched by ", User1,"'s submitted ProSite pattern, ignoring isoforms. \nWe will be using only the first 5 best matches to construct your phylogenetic tree.\n\n")
+for gene1 in striplist1:
+    print(gene1)
+    
+print("\n\nUSER 2: Here is the list of genes matched by ", User2,"'s submitted ProSite pattern, ignoring isoforms. \nWe will be using only the first 5 best matches to construct your phylogenetic tree.\n\n")
+for gene2 in striplist2:
+    print(gene2)
     
 print("\n\n\n")
+
+proceed = input("Please enter, 'Y' when ready to proceed with creation of alignments, distance matrices, and phylogenetic trees!")
 
 # Assign a variable to the first 5 results in the list of resulting genes
     
-User1Seq1 = striplist[0]
-User1Seq2 = striplist[1]
-User1Seq3 = striplist[2]
-User1Seq4 = striplist[3]
-User1Seq5 = striplist[4]
+User1Seq1 = striplist1[0]
+User1Seq2 = striplist1[1]
+User1Seq3 = striplist1[2]
+User1Seq4 = striplist1[3]
+User1Seq5 = striplist1[4]
+    
+User2Seq1 = striplist2[0]
+User2Seq2 = striplist2[1]
+User2Seq3 = striplist2[2]
+User2Seq4 = striplist2[3]
+User2Seq5 = striplist2[4]
 
 # Concatenate the User1 sequences into the uniprot.org FASTA formatted link, and open it in a new window,
 # to demonstrate the FASTA formatted sequences
     
-fastalink = ('https://www.uniprot.org/uniprot/?query='+User1Seq1+'%20OR%20'+User1Seq2+'%20OR%20'+User1Seq3+'%20OR%20%20'+User1Seq4+'%20OR%20%20'+User1Seq5+'&format=fasta&limit=5&sort=score')
+fastalink1 = ('https://www.uniprot.org/uniprot/?query='+User1Seq1+'%20OR%20'+User1Seq2+'%20OR%20'+User1Seq3+'%20OR%20%20'+User1Seq4+'%20OR%20%20'+User1Seq5+'&format=fasta&limit=5&sort=score')
+fastalink2 = ('https://www.uniprot.org/uniprot/?query='+User2Seq1+'%20OR%20'+User2Seq2+'%20OR%20'+User2Seq3+'%20OR%20%20'+User2Seq4+'%20OR%20%20'+User2Seq5+'&format=fasta&limit=5&sort=score')
 
 import webbrowser
 
-webbrowser.open_new(fastalink)
+# Optional: webbrowser.open_new(fastalink)
 
 # NEXT STEP: Write the content of the above link to a txt FASTA file
 # Using Beautiful Soup to do this!
 
-r1  = requests.get(fastalink)
+r1  = requests.get(fastalink1)
+r2  = requests.get(fastalink2)
 
 data1 = r1.text
+data2 = r2.text
 
 user1soup = str(BeautifulSoup(data1))
+user2soup = str(BeautifulSoup(data2))
 
 user1soup = re.sub('<html><head></head><body>&gt;', '', user1soup)
 user1soup = re.sub('</body></html>', '', user1soup)
+user2soup = re.sub('<html><head></head><body>&gt;', '', user2soup)
+user2soup = re.sub('</body></html>', '', user2soup)
 
-print("\n\nHere are your top 5 matched sequences in FASTA format, USER 1:\n")
+print("\n\nHere are your top 5 matched sequences in FASTA format, ", User1,":\n")
 print(user1soup)
 
+print("\n\nHere are your top 5 matched sequences in FASTA format, ", User2,":\n")
+print(user2soup)
+
+
     
-print("\nNow we will use this FASTA sequence to generate a phylogenetic tree\n\n")
+print("\nNow we will use each of", User1,"'s and", User2,"'s FASTA sequence sets to generate:\n ~ a multiple sequence alignment\n ~ a distance matrix, and\n ~ a phylogenetic tree!")
 
 
 text_file = open("User1FASTA.fsa", "w")
 n = text_file.write(user1soup)
 text_file.close()
 
-quit()
+text_file = open("User2FASTA.fsa", "w")
+n = text_file.write(user2soup)
+text_file.close()
